afdf92bb486d1e7cd22f02fad9f4947721430f72
lzy150015 <lzy150015@alibaba-inc.com>
2017-11-20 17:20:53 +0800

    Add project Eureka

---
+++ Eureka/runeureka.bat
@@ -0,0 +1,3 @@
+java -jar eureka-1.0-SNAPSHOT.jar --spring.profiles.active=peer1
+java -jar eureka-1.0-SNAPSHOT.jar --spring.profiles.active=peer2
+java -jar eureka-1.0-SNAPSHOT.jar --spring.profiles.active=peer3
---
+++ Eureka/src/main/java/study/discovery/EurekaApplication.java
@@ -0,0 +1,16 @@
+package study.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by banma-317 on 2017/11/13.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
